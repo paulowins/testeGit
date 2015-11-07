02873723f6e7f82036e0af8849acee3008e6bc95
paulowins <paulo.tec.informatica@gmail.com>
2015-11-06 23:25:36 -0200

    Initial commit

---
+++ app/src/main/java/jocimar_paulo/ifsp/testegit/MainActivity.java
@@ -0,0 +1,13 @@
+package jocimar_paulo.ifsp.testegit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
